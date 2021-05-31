9c7bec45ce7669725a1930dc0d03d80fd7bea574
Vincent METTON <vmetton@spha.fr>
2021-05-31 11:40:40 +0200

    :sparkles: Added refetch and called
    Author:    DaVinciLord <metton.vincent@gmail.com>

--- src/types.ts
+++ src/types.ts
@@ -17,15 +17,19 @@ export interface ErrorResponse {
 }
 export interface HookState {
   loading: boolean;
+  called: boolean;
   error: null | ErrorResponse;
   data: Sheet[];
+  refetch: () => void;
 }
 export enum ActionTypes {
   loading = 'LOADING',
   error = 'ERROR',
   success = 'SUCCESS',
+  called = 'CALLED',
 }
 export type Action =
   | { type: ActionTypes.loading; payload: HookState['loading'] }
+  | { type: ActionTypes.called; payload: HookState['called'] }
   | { type: ActionTypes.error; payload: HookState['error'] }
   | { type: ActionTypes.success; payload: HookState['data'] };
